b47e749e55ab5b37dc078be1e1a64170c15f9df0
Medamine-Bahassou <mohamedamine.bahassou@gmail.com>
2025-05-12 19:47:59 +0100

    shopify

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import utils.ali_express as ali_express
 pages = {
-    "Tools": [
+    "🔴 Ali-Express": [
         st.Page("tools/dashboard.py", title="Dashboard"),
         st.Page("tools/products.py", title="Products"),
-        st.Page("tools/chat_app.py", title="Chat"),
         st.Page("tools/machine_learning.py", title="Classifier (attractivness)"),
+        st.Page("tools/chat_app.py", title="Chat Bot"),
+    ],
+    "🟢 Shopify": [
+        st.Page("tools/shopify.py", title="Dashboard"),
     ]
+
 }
 
 
